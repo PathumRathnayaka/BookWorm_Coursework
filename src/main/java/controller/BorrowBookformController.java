47ee2022cd4fdaa00b75187307673d7e5ad2de4d
PathumRathnayaka <135231247+PathumRathnayaka@users.noreply.github.com>
2024-03-14 11:33:03 +0530

    BoorwBookController Implement

--- src/main/java/controller/BorrowBookformController.java
+++ src/main/java/controller/BorrowBookformController.java
@@ -1,4 +1,38 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.KeyEvent;
+
 public class BorrowBookformController {
+    @FXML
+    private TableColumn<?, ?> clmnBookID;
+
+    @FXML
+    private TableColumn<?, ?> clmnDate;
+
+    @FXML
+    private TableColumn<?, ?> clmnReturnDate;
+
+    @FXML
+    private TableColumn<?, ?> clmnTitle;
+
+    @FXML
+    private TableView<?> tblBorrowBook;
+
+    @FXML
+    private JFXTextField txtSearch;
+
+    @FXML
+    void imgKeyOnPress(KeyEvent event) {
+
+    }
+
+    @FXML
+    void txtSearchOnAction(ActionEvent event) {
+
+    }
 }
